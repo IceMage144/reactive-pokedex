42f456b74db4db0f5c11a828fb86963da1b32ff0
IceMage144 <jgabriel.basi@hotmail.com>
2018-06-21 22:35:11 -0300

    Add some async processing and display more information about pokemon

--- src/index.js
+++ src/index.js
@@ -5,65 +5,112 @@ import $ from 'jquery';
 
 const PKM_PER_PAGE = 20;
 
-function getJSON(url) {
+function getJSON(url, sync=true, fn=((response) => {})) {
     return ($.ajax({
         type: 'GET',
         url: url,
-        async: false,
+        async: !sync,
         dataType: 'json',
-        fail: function( jqXHR, textStatus, errorThrown ) {
+        complete: fn,
+        fail: ( jqXHR, textStatus, errorThrown ) => {
             console.log( 'Could not get posts, server response: ' + textStatus + ': ' + errorThrown );
         }
     })).responseJSON;
+}
+
+function firstUpper(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 class Pokedex extends React.Component {
     constructor(props) {
         super(props);
         let result = getJSON(`https://pokeapi.co/api/v2/pokemon/?limit=${PKM_PER_PAGE}&offset=0`);
-        let list = [];
-        for (const pkm of result.results)
-            list.push(pkm.name);
         this.state = {
             offset: 0,
-            pokemon: 0,
-            pkmList: list,
+            pkmList: result.results,
             pkmShow: (<div></div>),
             nextDisabled: false,
             prevDisabled: true
         };
         this.pkmMax = result.count;
-    }
-    showPokemon(i) {
-        this.setState({
-            offset: this.state.offset,
-            pokemon: this.state.offset + i + 1
+        this.cache = { 0: result.results };
+        console.log(result.results);
+        getJSON(`https://pokeapi.co/api/v2/pokemon/?limit=${3*PKM_PER_PAGE}&offset=${PKM_PER_PAGE}`,
+                             false, (response) => {
+            let pokemons = response.responseJSON.results;
+            for (let i = 0; i < 3; i++)
+                this.cache[PKM_PER_PAGE*(i+1)] = pokemons.slice(i*PKM_PER_PAGE, (i+1)*PKM_PER_PAGE);
         });
     }
-    renderPokemon() {
-        let result = {name: ""};
-        if (this.state.pokemon !== 0)
-            result = getJSON(`https://pokeapi.co/api/v2/pokemon/${this.state.pokemon}/`);
-        return (<div><h1>{result.name}</h1></div>);
+    renderPokemon(pkmNum) {
+        if (pkmNum !== 0) {
+            console.log(pkmNum);
+            getJSON(this.state.pkmList[pkmNum].url, false, (response) => {
+                let json = response.responseJSON;
+                console.log(json);
+                let types = [];
+                for (let i = 0; i < json.types.length; i++)
+                    types.push(<p key={i}>{json.types[i].type.name}</p>);
+                this.setState({
+                    pkmShow: (
+                        <div>
+                            <h1>{`#${json.id} ${firstUpper(json.name)}`}</h1>
+                            {types}
+                            <img src={json.sprites.front_default} alt={json.name}/>
+                            <p>Height: {json.height}</p>
+                            <p>Weight: {json.weight}</p>
+                            <div>
+                                <p>Speed: {json.stats[0].base_stat}</p>
+                                <p>Special-defense: {json.stats[1].base_stat}</p>
+                                <p>Special-attack: {json.stats[2].base_stat}</p>
+                                <p>Defense: {json.stats[3].base_stat}</p>
+                                <p>Attack: {json.stats[4].base_stat}</p>
+                                <p>HP: {json.stats[5].base_stat}</p>
+                            </div>
+                        </div>)
+                });
+            });
+            this.setState({
+                pkmShow: (<div><p>{"Loading..."}</p></div>)
+            });
+        }
+        else {
+            this.setState({
+                pkmShow: (<div></div>)
+            });
+        }
     }
     changeList(add) {
-        let nextOffset = this.state.offset + add*20;
-        let result = getJSON(`https://pokeapi.co/api/v2/pokemon/?limit=${PKM_PER_PAGE}&offset=${nextOffset}`);
-        let list = [];
-        for (const pkm of result.results)
-            list.push(pkm.name);
+        let nextOffset = this.state.offset + add*PKM_PER_PAGE;
+        let list = []
+        if (nextOffset in this.cache) {
+            list = this.cache[nextOffset];
+            let futureOffset = nextOffset + 3*PKM_PER_PAGE;
+            if (!(futureOffset in this.cache)) {
+                getJSON(`https://pokeapi.co/api/v2/pokemon/?limit=${PKM_PER_PAGE}&offset=${futureOffset}`,
+                                     false, (response) => {
+                    this.cache[futureOffset] = response.responseJSON.results;
+                });
+            }
+        }
+        else {
+            let result = getJSON(`https://pokeapi.co/api/v2/pokemon/?limit=${PKM_PER_PAGE}&offset=${nextOffset}`);
+            this.cache[nextOffset] = result.results;
+        }
         this.setState({
             pkmList: list,
             offset: nextOffset,
-            nextDisabled: (nextOffset + 20 >= this.pkmMax),
-            prevDisabled: (nextOffset - 20 < 0)
+            nextDisabled: (nextOffset + PKM_PER_PAGE >= this.pkmMax),
+            prevDisabled: (nextOffset - PKM_PER_PAGE < 0)
         });
-        console.log(nextOffset);
     }
     renderButton(i) {
         return (
             <li key={i}>
-                <button onClick={() => this.showPokemon(i)}>{this.state.pkmList[i]}</button>
+                <button onClick={() => this.renderPokemon(i)}>
+                    {firstUpper(this.state.pkmList[i].name)}
+                </button>
             </li>
         );
     }
@@ -78,7 +125,7 @@ class Pokedex extends React.Component {
                 </ul>
                 <button onClick={() => this.changeList(1)} disabled={this.state.nextDisabled}>Next</button>
                 <button onClick={() => this.changeList(-1)} disabled={this.state.prevDisabled}>Prev</button>
-                <div>{this.renderPokemon()}</div>
+                <div>{this.state.pkmShow}</div>
             </div>);
     }
 }
