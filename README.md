ee800bb7ea1256cf182998a1e1bce4bd6db5685f
IceMage144 <jgabriel.basi@hotmail.com>
2018-06-26 21:11:10 -0300

    Add a README and comment the code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 ## Reactive Pokédex
+
+This is a hiring challenge proposed by Intelie
+
+It consists of implementing a pokédex that:
+* Uses the API http://pokeapi.co for getting the data
+* Has a paginable list of pokémons
+* Displays a detailed view of the pokémon when clicking at it
--- src/index.js
+++ src/index.js
@@ -3,28 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import $ from 'jquery';
 
+// Pokemons per page
 const PKM_PER_PAGE = 20;
 
+/*
+ * Gets a json through "url" and returns it. This function can be executed at
+ * asynchronous mode if "sync" is false and a function "fn" can be executed
+ * right after the function recieves a response.
+ */
 function getJSON(url, sync=true, fn=((response) => {})) {
     return ($.ajax({
         type: 'GET',
         url: url,
         async: !sync,
         dataType: 'json',
-        complete: fn,
-        fail: ( jqXHR, textStatus, errorThrown ) => {
-            console.log( 'Could not get posts, server response: ' + textStatus + ': ' + errorThrown );
-        }
+        complete: fn
     })).responseJSON;
 }
 
+/*
+ * Capitalizes the string "string"
+ */
 function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+/*
+ * Pokedex class that is rendered at the page
+ */
 class Pokedex extends React.Component {
     constructor(props) {
         super(props);
+
+        // Send a request to pokeapi to get the first page
         let result = getJSON(`https://pokeapi.co/api/v2/pokemon/?limit=${PKM_PER_PAGE}&offset=0`);
         this.state = {
             offset: 0,
@@ -33,9 +44,10 @@ class Pokedex extends React.Component {
             nextDisabled: false,
             prevDisabled: true
         };
-        this.requesting = false;
         this.pkmMax = result.count;
         this.cache = { 0: result.results };
+
+        // Request more pages for caching
         getJSON(`https://pokeapi.co/api/v2/pokemon/?limit=${3*PKM_PER_PAGE}&offset=${PKM_PER_PAGE}`,
                 false, (response) => {
             let pokemons = response.responseJSON.results;
@@ -43,7 +55,12 @@ class Pokedex extends React.Component {
                 this.cache[PKM_PER_PAGE*(i+1)] = pokemons.slice(i*PKM_PER_PAGE, (i+1)*PKM_PER_PAGE);
         });
     }
+
+    /*
+     * Render the pokemon at the "pkmNum" position of this.state.pkmList
+     */
     renderPokemon(pkmNum) {
+        // Request pokemon's data
         getJSON(this.state.pkmList[pkmNum].url, false, (response) => {
             let json = response.responseJSON;
             let types = [];
@@ -69,40 +86,32 @@ class Pokedex extends React.Component {
                             <p>Attack: {json.stats[4].base_stat}</p>
                             <p>HP: {json.stats[5].base_stat}</p>
                         </div>
-                    </div>)
+                    </div>
+                )
             });
         });
+        // Add a pleceholder to indicate that no response has come yet
         this.setState({
             pkmShow: (
                 <div className="screen">
                     <p>{"Loading..."}</p>
-                </div>)
+                </div>
+            )
         });
     }
+
+    /*
+     * Change pokemon list to the next page if "add" equals 1 or to the
+     * previous, if equal to -1
+     */
     changeList(add) {
         let nextOffset = this.state.offset + add*PKM_PER_PAGE;
-        if (nextOffset > this.pkmMax) {
-            console.log("Failed...");
+        if (nextOffset >= this.pkmMax || nextOffset < 0)
             return;
-        }
         let list = [];
-        console.log(`${this.state.offset} ${nextOffset}`);
+        // Checks if the page is at the cache, and fails to load, otherwise
         if (nextOffset in this.cache) {
             list = this.cache[nextOffset];
-            console.log(`Found ${nextOffset} at cache!`);
-            let futureOffset = nextOffset + 3*PKM_PER_PAGE;
-            if (futureOffset < this.pkmMax && !(futureOffset in this.cache)) {
-                console.log(`Requesting ${futureOffset}...`);
-                getJSON(`https://pokeapi.co/api/v2/pokemon/?limit=${PKM_PER_PAGE}&offset=${futureOffset}`,
-                        false, (response) => {
-                    if (!(futureOffset in this.cache)) {
-                        this.cache[futureOffset] = response.responseJSON.results;
-                        console.log(`${futureOffset} arrived from first request`);
-                    }
-                    else
-                        console.log(`${futureOffset} arrived from first request and could not use`);
-                });
-            }
             this.setState({
                 pkmList: list,
                 offset: nextOffset,
@@ -110,7 +119,20 @@ class Pokedex extends React.Component {
                 prevDisabled: (nextOffset - PKM_PER_PAGE < 0)
             });
         }
+        // Load more pages to cache
+        let futureOffset = nextOffset + 3*PKM_PER_PAGE;
+        if (futureOffset < this.pkmMax && !(futureOffset in this.cache)) {
+            getJSON(`https://pokeapi.co/api/v2/pokemon/?limit=${PKM_PER_PAGE}&offset=${futureOffset}`,
+                    false, (response) => {
+                if (!(futureOffset in this.cache))
+                    this.cache[futureOffset] = response.responseJSON.results;
+            });
+        }
     }
+
+    /*
+     * Render a list button for the i-th pokemon of this.state.pkmList
+     */
     renderButton(i) {
         if (!(i in this.state.pkmList))
             return (<li key={i}></li>);
@@ -122,6 +144,10 @@ class Pokedex extends React.Component {
             </li>
         );
     }
+
+    /*
+     * Default render method from React that renders everything
+     */
     render() {
         let list = []
         for (let i = 0; i < PKM_PER_PAGE; i++)
